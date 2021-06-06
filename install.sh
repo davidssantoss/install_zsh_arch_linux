9a2a83e0ff79c6ed088b7ca8de5d6c6dfb8a2abf
devsantoss <davidsteven.santos9@gmail.com>
2021-06-06 14:08:00 -0500

    Adding function to customizate zsh

--- install.sh
+++ install.sh
@@ -4,11 +4,22 @@
 # ¡¡¡    No ejecutar el script con el usuario root !!!
 
 ############################################
-# Installation of zsh in Arch Linux
-# Instalacion de zsh en Arch Linux
+# Installation and customization of zsh in Arch Linux
+# Instalacion y personalizacion de zsh en Arch Linux
 # @author: devsantos
 ############################################
 
+cat << 'EOF'
+
+   _           _   _              _     
+  (_) __ _ ___| |_(_) ___ _______| |__  
+  | |/ _` / __| __| |/ __|_  / __| '_ \ 
+  | | (_| \__ \ |_| | (__ / /\__ \ | | |
+ _/ |\__,_|___/\__|_|\___/___|___/_| |_|
+|__/                                    
+... just another script to install and customizate zsh
+
+EOF
 
 # Variables
 uid=$(id -u)
@@ -20,9 +31,56 @@ fi
 
 echo "Preparing the installation..."
 
-#Update, Download and upgrade packages and install zsh
-#Actualizar, descargar e instalar paquetes y instalar zsh
-sudo pacman -Syu zsh
+#Update, Download and upgrade packages also install zsh and git
+#Actualizar, descargar e instalar paquetes tambien instalar zsh y git
+sudo pacman -Syu base-devel zsh git 
+
+# Customizing zsh
+
+install_ohmyzsh() {
+    clear
+    cd ~
+    mkdir ohmyzsh-git && cd ohmyzsh-git
+    git clone https://github.com/ohmyzsh/ohmyzsh.git
+    cd ohmyzsh/tools
+    sed -i "s/exec.*/#exec zsh -l/" install.sh
+    sh install.sh
+}
+install_powerlevel10k() {    
+    git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
+    sed -i 's@^ZSH_THEME=.*$@ZSH_THEME="powerlevel10k/powerlevel10k"@g' ~/.zshrc            
+}
+
+read -n 1 -p "Would you like add customizations in zsh rigth now? :>[Y/n] " ans 
+if [[ $ans == 'Y' || $ans == 'y' || $ans == 'S' || $ans == 's' || $ans == '' ]]; then
+    clear
+    echo -e "\t ***Install customization***"
+    echo -e "\n* Do you want to install oh-my-zsh? Type 1"
+    echo -e "* Do you want to install oh-my-zsh with theme powerlevel10k? Type 2"
+    echo -e "* By default will install oh-my-zsh"
+    echo -e "* Do not customizate! Type 0\n\n"
+    read -n 1 -p "Choose your option [1,2,0]:> " option
+    case $option  in
+        1)
+            install_ohmyzsh
+            echo "done!"
+            ;;
+        2)
+     		install_ohmyzsh
+            install_powerlevel10k
+            echo "done!"
+            ;;
+        0)
+     		echo "not customized"
+            ;;
+        "")
+            install_ohmyzsh
+            ;;
+        *)
+            true
+            ;;
+    esac
+fi
 
 # Change shell to zsh
 # Cambiar de shell a zsh
